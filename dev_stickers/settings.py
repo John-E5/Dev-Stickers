22077b22293f75b74a14e25b7221cb7a4437e84d
John <johnennis5@gmail.com>
2019-08-03 22:02:23 +0100

    Added accounts app and updated urls.py

--- dev_stickers/settings.py
+++ dev_stickers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&)0^j!m4rafkf8cg4c-s#85q@$8k4i!k=!pgld=fc$$wb$+&&&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
